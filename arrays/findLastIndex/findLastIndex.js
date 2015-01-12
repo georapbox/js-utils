533a44849759ddc2e049e2f5064095763c6fbfba
georapbox <georapbox@gmail.com>
2015-01-12 10:32:43 +0200

    - annotate function declaration

--- arrays/findLastIndex/findLastIndex.js
+++ arrays/findLastIndex/findLastIndex.js
@@ -1,3 +1,10 @@
+/**
+ * Returns the index of the first element of a collection that passes the callback check,
+ * iterating the elements from right to left.
+ * @param   {Array}    array    The array to check.
+ * @param   {Function} callback The function called per iteration.
+ * @returns {Number}   The index of the first element that matches the callabck check.
+ */
 function findLastIndex(array, callback) {
     var length = array ? array.length : 0;
 
