a2a9c11f0210933bbe33a35daa7a6e47dd4d5379
georapbox <georapbox@gmail.com>
2015-02-19 11:06:50 +0200

    Add tests

---
+++ src/arrays/compact/tests/specs.js
@@ -0,0 +1,22 @@
+/*global compact*/
+
+(function () {
+    'use strict';
+
+    describe('compact', function () {
+        var arr = [0, 1, false, 2, '', 3, null];
+
+        it('tracks all the arguments of its calls', function () {
+            spyOn(window, 'compact');
+            compact(arr);
+            expect(window['compact']).toHaveBeenCalledWith(arr);
+        });
+
+        it('expects the values of the result array to be truthy', function () {
+            var ca = compact(arr);
+            for (var i = 0, l = ca.length; i < l; i++) {
+                expect(ca[i]).toBeTruthy();
+            }
+        });
+    });
+}());
