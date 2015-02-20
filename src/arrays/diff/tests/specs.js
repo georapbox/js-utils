7120cbe72c3bf2c93c8345bd62f813786fc4a817
georapbox <georapbox@gmail.com>
2015-02-20 10:20:07 +0200

    Add tests

---
+++ src/arrays/diff/tests/specs.js
@@ -0,0 +1,20 @@
+/*global diff*/
+
+(function () {
+    'use strict';
+
+    describe('diff', function () {
+        var a = [1, 2, 3, 4, {}],
+            b = [5, 2, 10];
+
+        it('tracks all the arguments of its calls', function () {
+            spyOn(window, 'diff');
+            diff(a, b);
+            expect(window.diff).toHaveBeenCalledWith(a, b);
+        });
+
+        it('expects the result array to be "[1, 3, 4, {}]"', function () {
+            expect(diff(a, b)).toEqual([1, 3, 4, {}]);
+        });
+    });
+}());
