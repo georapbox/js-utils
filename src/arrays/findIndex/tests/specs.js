b100796326013df06cc7230059c340c4e60977e7
graptis <graptis@customedialabs.com>
2015-02-20 17:51:30 +0200

    Add tests

---
+++ src/arrays/findIndex/tests/specs.js
@@ -0,0 +1,21 @@
+/*global findIndex*/
+
+(function () {
+    'use strict';
+
+    describe('arrays/findIndex', function () {
+        var arr = ['a', 'b', 'c', 'd', 'e'];
+
+		it('index of "b" should be 1 in array [\'a\', \'b\', \'c\', \'d\', \'e\']', function () {
+            expect(findIndex(arr, function (chr) {
+				return chr === 'b';
+			})).toBe(1);
+        });
+
+		it('index of "Hello" should be -1 in array [\'a\', \'b\', \'c\', \'d\', \'e\']', function () {
+			expect(findIndex(arr, function (chr) {
+				return chr === 'Hello';
+			})).toBe(-1);
+		});
+    });
+}());
---
+++ src/arrays/uniq/tests/specs.js
@@ -0,0 +1,22 @@
+/*global uniq*/
+
+(function () {
+    'use strict';
+
+    describe('arrays/uniq', function () {
+		var arr = ['a', 'a', 'b', true, '2', 2, false, true];
+
+		it('should contain only one occurence of "a"', function () {
+			var uArr = uniq(arr),
+				count = 0;
+
+			for (var i = 0, len = arr.length; i < len; i++) {
+				if (uArr[i] === 'a') {
+					count += 1;
+				}
+			}
+
+			expect(count).toEqual(1);
+		});
+    });
+}());
