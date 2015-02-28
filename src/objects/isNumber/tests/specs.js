29587f84e8efbeb1c06bb9be1e898dd3b093b86a
georapbox <georapbox@gmail.com>
2015-02-28 20:48:28 +0200

    Add tests to isNumber()

---
+++ src/objects/isNumber/tests/specs.js
@@ -0,0 +1,19 @@
+/*global isNumber*/
+
+(function () {
+    'use strict';
+
+    describe('objects/isNumber', function () {
+        it('isNumber(404) should return true', function () {
+            expect(isNumber(404)).toEqual(true);
+        });
+
+        it('isNumber(new Date().getTime()) should return true', function () {
+            expect(isNumber(new Date().getTime())).toEqual(true);
+        });
+
+        it('isNumber(\'302\') should return false', function () {
+            expect(isNumber('302')).toEqual(false);
+        });
+    });
+}());
