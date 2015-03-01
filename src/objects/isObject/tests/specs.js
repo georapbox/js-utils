454a19582c76ba604d70ae9721d6cf7d41d6680c
georapbox <georapbox@gmail.com>
2015-03-01 20:48:24 +0200

    Add tests to isObject()

---
+++ src/objects/isObject/tests/specs.js
@@ -0,0 +1,27 @@
+/*global isObject*/
+
+(function () {
+    'use strict';
+
+    describe('objects/isObject', function () {
+        it('isObject({}) should return true', function () {
+            expect(isObject({})).toEqual(true);
+        });
+
+        it('isObject(function test() {}) should return true', function () {
+            expect(isObject(function test() {})).toEqual(true);
+        });
+
+        it('isObject([1, 2, 3]) should return true', function () {
+            expect(isObject([1, 2, 3])).toEqual(true);
+        });
+
+        it('isObject(new String(\'Hello world\')) should return true', function () {
+            expect(isObject(new String('Hello world'))).toEqual(true);
+        });
+
+        it('isObject(\'Hello world\') should return false', function () {
+            expect(isObject('Hello world')).toEqual(false);
+        });
+    });
+}());
