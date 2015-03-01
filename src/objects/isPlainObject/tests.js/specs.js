6743fe1b89b93ba2cf5880dcfc629f9958638e1e
georapbox <georapbox@gmail.com>
2015-03-01 20:56:51 +0200

    Add tests to isPlainObject()

---
+++ src/objects/isPlainObject/tests.js/specs.js
@@ -0,0 +1,31 @@
+/*global isPlainObject*/
+
+(function () {
+    'use strict';
+
+    describe('objects/isPlainObject', function () {
+        function Foo() {
+            this.foo = 'bar';
+        }
+
+        it('isPlainObject({foo: \'bar\'}) should return true', function () {
+            expect(isPlainObject({foo: 'bar'})).toEqual(true);
+        });
+
+        it('isPlainObject(Object.create({})) should return true', function () {
+            expect(isPlainObject(Object.create({}))).toEqual(true);
+        });
+
+        it('isPlainObject(new Foo()) should return false', function () {
+            expect(isPlainObject(new Foo())).toEqual(false);
+        });
+
+        it('isPlainObject(Object.create(null)) should return false', function () {
+            expect(isPlainObject(Object.create(null))).toEqual(false);
+        });
+
+        it('isPlainObject([1, 2, 3]) should return false', function () {
+            expect(isPlainObject([1, 2, 3])).toEqual(false);
+        });
+    });
+}());
