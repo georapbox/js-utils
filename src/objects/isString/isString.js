2593c565529e35ee6dcc9b09f88a2af7e62952c5
georapbox <georapbox@gmail.com>
2015-03-22 08:04:36 +0200

    Update docs

--- src/objects/isString/isString.js
+++ src/objects/isString/isString.js
@@ -1,7 +1,7 @@
 /**
  * Checks if 'value' is string.
- * @param   {*}       'value' to check if is string.
- * @returns {Boolean} Returns true if string, else false.
+ * @param {*} 'value' to check if is string.
+ * @returns {boolean} Returns true if string, else false.
  */
 function isString(value) {
     'use strict';
