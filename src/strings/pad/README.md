c83f40184a96f0c689e3976aa1069e1d97d3a249
georapbox <georapbox@gmail.com>
2015-03-30 10:35:24 +0300

    [Strings] pad()

---
+++ src/strings/pad/README.md
@@ -0,0 +1,83 @@
+###[Strings](../) > pad
+
+```js
+pad(str, len [, ch])
+```
+
+> Pad <code>str</code> on **both** sides to the given <code>len</code>, with optional <code>ch</code> defaulting to a space.
+
+- <code>str {string}</code>
+
+The string to process.
+
+- <code>len {number}</code>
+
+The length of the padding.
+
+- <code>[ch] {string|number}</code>
+
+The character(s) to use for padding. Default is space.
+
+####Example
+```js
+pad('Hello', 9); // => "  Hello  "
+pad('Hello', 9, '-'); // => "--Hello--"
+pad('Hello', 6, '-'); // => "-Hello"
+pad('Hello', 5, '-'); // => "Hello" (len is the same as str.length)
+```
+
+---
+
+###[Strings](../) > padLeft
+
+```js
+pad(str, len [, ch])
+```
+
+> Pad <code>str</code> on **left** side to the given <code>len</code>, with optional <code>ch</code> defaulting to a space.
+
+- <code>str {string}</code>
+
+The string to process.
+
+- <code>len {number}</code>
+
+The length of the padding.
+
+- <code>[ch] {string|number}</code>
+
+The character(s) to use for padding. Default is space.
+
+####Example
+```js
+padLeft('Hello', 10); // => "-----Hello"
+padLeft('Hello', 5, '-'); // => "Hello"
+```
+
+---
+
+###[Strings](../) > padRight
+
+```js
+pad(str, len [, ch])
+```
+
+> Pad <code>str</code> on **right** side to the given <code>len</code>, with optional <code>ch</code> defaulting to a space.
+
+- <code>str {string}</code>
+
+The string to process.
+
+- <code>len {number}</code>
+
+The length of the padding.
+
+- <code>[ch] {string|number}</code>
+
+The character(s) to use for padding. Default is space.
+
+####Example
+```js
+padRight('Hello', 10); // => "Hello-----"
+padRight('Hello', 5, '-'); // => "Hello"
+```
---
+++ src/strings/pad/pad.js
@@ -0,0 +1,72 @@
+/**
+ * Pad "str" on both sides to the given "len", with optional "ch" defaulting to a space.
+ * @param {string} str The string to process.
+ * @param {number} len The length to pad the string.
+ * @param {string|number} [ch] The character(s) to use for padding. Default is space.
+ * @returns {string} The processed string.
+ */
+function pad(str, len, ch) {
+    'use strict';
+
+    ch = ch || ' '; // Set default character.
+
+    if (str.length >= len) {
+        return str;
+    }
+
+    len = len - str.length;
+
+    var left = [],
+        right = [];
+
+    left.length = Math.ceil(len / 2) + 1;
+    right.length = Math.floor(len / 2) + 1;
+    left = left.join(ch);
+    right = right.join(ch);
+
+    return left + str + right;
+}
+
+/**
+ * Pad "str" on left side to the given "len", with optional "ch" defaulting to a space.
+ * @param {string} str The string to process.
+ * @param {number} len The length to pad the string.
+ * @param {string|number} [ch] The character(s) to use for padding. Default is space.
+ * @returns {string} The processed string.
+ */
+function padLeft(str, len, ch) {
+    'use strict';
+
+    ch = ch || ' '; // Set default character.
+
+    if (str.length >= len) {
+        return str;
+    }
+
+    var left = [];
+    left.length = len - str.length + 1;
+    left = left.join(ch);
+    return left + str;
+}
+
+/**
+ * Pad "str" on right side to the given "len", with optional "ch" defaulting to a space.
+ * @param {string} str The string to process.
+ * @param {number} len The length to pad the string.
+ * @param {string|number} [ch] The character(s) to use for padding. Default is space.
+ * @returns {string} The processed string.
+ */
+function padRight(str, len, ch) {
+    'use strict';
+
+    ch = ch || ' '; // Set default character.
+
+    if (str.length >= len) {
+        return str;
+    }
+
+    var right = [];
+    right.length = len - str.length + 1;
+    right = right.join(ch);
+    return str + right;
+}
