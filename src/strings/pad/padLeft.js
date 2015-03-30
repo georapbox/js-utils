e3d93ecb5a4b5bffaabef3ac788cb97b86fe343f
georapbox <georapbox@gmail.com>
2015-03-30 10:46:21 +0300

    Seperate pad, padLeft, padRight to own files

--- src/strings/pad/padLeft.js
+++ src/strings/pad/padLeft.js
@@ -1,11 +1,11 @@
 /**
- * Pad "str" on both sides to the given "len", with optional "ch" defaulting to a space.
+ * Pad "str" on left side to the given "len", with optional "ch" defaulting to a space.
  * @param {string} str The string to process.
  * @param {number} len The length to pad the string.
  * @param {string|number} [ch] The character(s) to use for padding. Default is space.
  * @returns {string} The processed string.
  */
-function pad(str, len, ch) {
+function padLeft(str, len, ch) {
     'use strict';
 
     ch = ch || ' '; // Set default character.
@@ -14,15 +14,8 @@ function pad(str, len, ch) {
         return str;
     }
 
-    len = len - str.length;
-
-    var left = [],
-        right = [];
-
-    left.length = Math.ceil(len / 2) + 1;
-    right.length = Math.floor(len / 2) + 1;
+    var left = [];
+    left.length = len - str.length + 1;
     left = left.join(ch);
-    right = right.join(ch);
-
-    return left + str + right;
+    return left + str;
 }
