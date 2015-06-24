58b799097f6713d688263e8576782941462f15d6
georapbox <georapbox@gmail.com>
2015-06-24 10:19:23 +0300

    randomString tests

---
+++ src/strings/randomString/tests/specs.js
@@ -0,0 +1,15 @@
+/*global randomString*/
+
+(function () {
+    'use strict';
+
+    describe('strings/randomString', function () {
+        it('generates a random string of 10 characters', function () {
+            expect(randomString(10).length).toEqual(10);
+        });
+
+        it('generates a random string of 0 characters if both arguments omitted', function () {
+            expect(randomString().length).toEqual(0);
+        });
+    });
+}());
