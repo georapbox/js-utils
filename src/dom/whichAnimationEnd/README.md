5319a560a70c7972b9ce130b11665e2d207d2fa1
georapbox <georapbox@gmail.com>
2015-07-18 07:44:04 +0300

    whichAnimationEnd function

---
+++ src/dom/whichAnimationEnd/README.md
@@ -0,0 +1,8 @@
+### [DOM](../) > whichAnimationEnd
+
+```js
+whichAnimationEnd()
+```
+
+> Detects the supported property name for the "animationend" event.
+> Returns the supported property name (animationend, oAnimationEnd, webkitAnimationEnd) or <code>undefined</code> if not supported.
---
+++ src/dom/whichAnimationEnd/whichAnimationEnd.js
@@ -0,0 +1,25 @@
+/**
+ * Detects the supported property name for the "animationend" event.
+ * @returns {string} The supported property name or undefined if not supported.
+ */
+function whichAnimationEnd() {
+    'use strict';
+
+    var key,
+        el = document.createElement('div'),
+        animations = {
+            'animation': 'animationend',
+            'OAnimation': 'oAnimationEnd',
+            'MozAnimation': 'animationend',
+            'WebkitAnimation': 'webkitAnimationEnd'
+        };
+
+    for (key in animations) {
+        if (animations.hasOwnProperty(key)) {
+            if (el.style[key] !== undefined) {
+                el = null;
+                return animations[key];
+            }
+        }
+    }
+}
