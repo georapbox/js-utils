a701b0fb2bd2008aef1fdb0a56ddccb8f7331f91
georapbox <georapbox@gmail.com>
2015-10-09 08:23:35 +0300

    Fix to return true for NaN

--- src/objects/isNumber/isNumber.js
+++ src/objects/isNumber/isNumber.js
@@ -1,13 +1,13 @@
 /**
  * Checks if 'value' is a number.
  * Note: NaN is considered a number.
- * @param   {*}       value The value to check.
- * @returns {Boolean} Returns true if 'value' is a number, else false.
+ * @param {*} value The value to check.
+ * @return {Boolean} Returns true if 'value' is a number, else false.
  */
 function isNumber(value) {
     'use strict';
 
-    return value &&
+    return value != null && // NOTE: Using non strict equality to check against null and undefined.
         Object.prototype.toString.call(value) === '[object Number]' &&
         typeof value === 'number' ||
         false;
