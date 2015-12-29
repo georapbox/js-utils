18102af667e4b1d12a5ee917a55370bd6ecf9bb2
georapbox <georapbox@gmail.com>
2015-12-29 10:23:02 +0200

    remove unecessary check

--- src/is/isDate/isDate.js
+++ src/is/isDate/isDate.js
@@ -1,13 +1,9 @@
 /**
  * Checks if 'value' is a date.
- * @param   {*}       value The value to check.
+ * @param {*} value The value to check.
  * @returns {Boolean} Returns true if 'value' is a date, else returns false.
  */
 function isDate(value) {
     'use strict';
-
-    return value &&
-        typeof value === 'object' &&
-        Object.prototype.toString.call(value) === '[object Date]' ||
-        false;
+    return typeof value === 'object' && Object.prototype.toString.call(value) === '[object Date]';
 }
