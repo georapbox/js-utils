329044673e285b9e8f9504532c5ea14f2554670f
georapbox <georapbox@gmail.com>
2015-12-29 16:53:26 +0200

    Fix typo in annotation

--- src/is/isNull/isNull.js
+++ src/is/isNull/isNull.js
@@ -1,7 +1,7 @@
 /**
  * Checks if 'value' is null.
  * @param {*} value The value to check.
- * @returns {boolean} Returns true if the 'value' is null value, else false.
+ * @returns {boolean} Returns true if the 'value' is null, else false.
  */
 function isNull(value) {
     'use strict';
