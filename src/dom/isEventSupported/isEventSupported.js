f121b3285cbae7cfe511145c5f37d9fad3502763
George Raptis <georapbox@gmail.com>
2016-02-23 13:54:14 +0200

    [ci skip] Add comments

--- src/dom/isEventSupported/isEventSupported.js
+++ src/dom/isEventSupported/isEventSupported.js
@@ -9,14 +9,22 @@ var isEventSupported = (function (win, doc, undefined) {
         error: 'img',
         load: 'img',
         abort: 'img'
-    };
-    var cache = {};
+    },
+    cache = {};
 
+    /**
+     * Checks if an event is supported in a browser environment.
+     * @param {String} eventName The event to check if is supported.
+     * @param {Object} [element] An HTML node to check if an event is supported on.
+     *        Some events are supported on specific elements, eg `resize` is supported on `window` but not on a `div` element.
+     *        If omited the results are cached and next calls with the same `eventName` will return the results from cache.
+     * @return {Boolean} True if event is supported, else false.
+     */
     function isEventSupported(eventName, element) {
-        var canCache = (arguments.length === 1); // Cache when no element is given
-        var isSupported;
+        var canCache = !element,
+            isSupported;
 
-        // Return the cached result if exists
+        // Return the cached result if exists.
         if (canCache && cache[eventName]) {
             return cache[eventName];
         }
@@ -25,8 +33,10 @@ var isEventSupported = (function (win, doc, undefined) {
         eventName = 'on' + eventName;
         isSupported = eventName in element;
 
+        // Old Gecko based browsers create methods on an element when an attribute
+        // with the name corresponding to a “known” event is set on that element.
         if (!isSupported) {
-            // If it has no `setAttribute` (i.e. doesn't implement Node interface), try a generic element like `div`
+            // If it has no `setAttribute` (i.e. doesn't implement Node interface), try a generic element like `div`.
             if (!element.setAttribute) {
                 element = doc.createElement('div');
             }
