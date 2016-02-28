6c506a36df74f90ebc9e970e9cb695251e5dc781
georapbox <georapbox@gmail.com>
2016-02-28 16:05:31 +0200

    Add tests

---
+++ src/misc/curry/tests/specs.js
@@ -0,0 +1,28 @@
+/*global curry*/
+
+(function () {
+    'use strict';
+
+    describe('misc/curry', function () {
+        var sum = function (a, b, c) {
+            return a + b + c;
+        },
+        curriedSum = curry(sum, 3),
+        sumTwo = curriedSum(2),
+        sumThree = sumTwo(3),
+        finalSum = sumThree(5);
+
+        it('should return a curried function until all expected arguments are satisfied', function () {
+            expect(typeof sumTwo).toBe('function');
+            expect(typeof sumThree).toBe('function');
+            expect(typeof finalSum).toBe('number');
+            expect(finalSum).toEqual(10);
+        });
+
+        it('should throw error if supply more arguments than expected', function () {
+            expect(function () {
+                return finalSum(10);
+            }).toThrow();
+        });
+    });
+}());
