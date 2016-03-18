4a8dad7baca5303373930f09b72226d14d50e321
graptis@customedialabs.com <georapbox@gmail.com>
2016-03-18 18:55:07 +0200

    [ci skip] Annotate function

--- src/strings/subStrBeforeChars/subStrBeforeChars.js
+++ src/strings/subStrBeforeChars/subStrBeforeChars.js
@@ -1,3 +1,13 @@
+/**
+ * Returns a substring before a specific sequence of character(s).
+ * By default looks for the first occuerence of this sequence.
+ * If the character or sequence of characters not found, returns the initial string.
+ *
+ * @param {string} str The string to process.
+ * @param {string} chars The seqquence of characters to use as delimiter.
+ * @param {boolean} [last] If "true" or any truthy value, will look for the last occurence of the characters specified.
+ * @returns {string} The final string.
+ */
 function subStrBeforeChars(str, chars, last) {
     'use strict';
 
