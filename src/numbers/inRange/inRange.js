791f7b072760805866311454d119c15a3223b2f3
George Raptis <georapbox@gmail.com>
2016-03-24 21:28:26 +0200

    [ci skip] inRange function

---
+++ src/numbers/inRange/inRange.js
@@ -0,0 +1,21 @@
+/**
+ * Checks if a number is between start and up to but not including, end.
+ * If start is greater than end the params are swapped to support negative ranges.
+ * @param {Number} num The number to check.
+ * @param {Number} start The start of the range.
+ * @param {Number} end The end of the range.
+ * @return {Boolean} Returns true if number is in the range, else false.
+ */
+function inRange(num, start, end) {
+    'use strict';
+
+    var isNaN = function (value) {
+        return value !== value;
+    };
+
+    if (isNaN(parseInt(num, 10)) || isNaN(parseInt(start, 10)) || isNaN(parseInt(end, 10))) {
+        throw new TypeError('Arguments must be numbers');
+    }
+
+    return num >= Math.min(start, end) && num < Math.max(start, end);
+}
