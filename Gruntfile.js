47c8d3a91d571f33c63aa6065af6a9ccc29e7f47
George Raptis <georapbox@gmail.com>
2016-03-29 19:04:34 +0300

    [ci skip] Stop tracking updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,68 +5,68 @@ var pkg = require('./package'),
 module.exports = function (grunt) {
     'use strict';
 
-	grunt.initConfig({
+    grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-		md2html: {
-			one_file: {
-				options: {
-					basePath: './docs-layout/',
-					layout: 'docs-layout/layout.html',
-					templateData: {
-						basename: function (src) {
-							return src.substr(src.lastIndexOf(path.sep) + 1);
-						},
+        md2html: {
+            one_file: {
+                options: {
+                    basePath: './docs-layout/',
+                    layout: 'docs-layout/layout.html',
+                    templateData: {
+                        basename: function (src) {
+                            return src.substr(src.lastIndexOf(path.sep) + 1);
+                        },
                         innerPage: false,
-						path: '../docs-layout',
-						author: '<%= pkg.author %>',
-						appName: '<%= pkg.name %>',
+                        path: '../docs-layout',
+                        author: '<%= pkg.author %>',
+                        appName: '<%= pkg.name %>',
                         version: '<%= pkg.version %>',
                         description: '<%= pkg.description %>',
-						homepage: "'<%= pkg.homepage %>'",
+                        homepage: "'<%= pkg.homepage %>'",
                         repository: "<%= pkg.repository.url %>",
                         license: "<%= pkg.license %>"
-					}
-				},
-				files: [{
-					src: 'README.md',
-					dest: 'docs/index.html'
-				},{
-					src: 'CHANGELOG.md',
-					dest: 'docs/CHANGELOG.html'
-				}]
-			},
-    		multiple_files: {
-				options: {
-					basePath: './docs-layout/',
-					layout: 'docs-layout/layout.html',
-					templateData: {
-						basename: function (src) {
-							return src.substr(src.lastIndexOf(path.sep) + 1);
-						},
+                    }
+                },
+                files: [{
+                    src: 'README.md',
+                    dest: 'docs/index.html'
+                },{
+                    src: 'CHANGELOG.md',
+                    dest: 'docs/CHANGELOG.html'
+                }]
+            },
+            multiple_files: {
+                options: {
+                    basePath: './docs-layout/',
+                    layout: 'docs-layout/layout.html',
+                    templateData: {
+                        basename: function (src) {
+                            return src.substr(src.lastIndexOf(path.sep) + 1);
+                        },
                         innerPage: true,
-						path: '../../../../docs-layout',
-						author: '<%= pkg.author %>',
-						appName: '<%= pkg.name %>',
+                        path: '../../../../docs-layout',
+                        author: '<%= pkg.author %>',
+                        appName: '<%= pkg.name %>',
                         version: '<%= pkg.version %>',
                         description: '<%= pkg.description %>',
-						homepage: "'<%= pkg.homepage %>'",
+                        homepage: "'<%= pkg.homepage %>'",
                         repository: "<%= pkg.repository.url %>",
                         license: "<%= pkg.license %>"
-					}
-				},
-				files: [{
-					expand: true,
-					cwd: 'src',
-					src: ['**/*.md'],
-					dest: 'docs/src/',
+                    }
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src',
+                    src: ['**/*.md'],
+                    dest: 'docs/src/',
                     ext: '.html',
                     rename: function(dest, src) {
                         return dest + src.replace('README','index');
                     }
-				}]
-			}
-		}
+                }]
+            }
+        }
     });
 
     // Load plugins.
@@ -80,20 +80,19 @@ module.exports = function (grunt) {
     );
 
     grunt.registerTask('postinstall', 'Run this after npm install', function () {
-		console.log(chalk.blue("   _     ______                    _   _       _"));
-		console.log(chalk.blue("  (_)   |  ____|                  | | (_)     | |"));
-		console.log(chalk.blue("   _ ___| |__   ___ ___  ___ _ __ | |_ _  __ _| |___"));
-		console.log(chalk.blue("  | / __|  __| / __/ __|/ _ \\ '_ \\| __| |/ _` | / __|"));
-		console.log(chalk.blue("  | \\__ \\ |____\\__ \\__ \\  __/ | | | |_| | (_| | \\__ \\"));
-		console.log(chalk.blue("  | |___/______|___/___/\\___|_| |_|\\__|_|\\__,_|_|___/"));
-		console.log(chalk.blue(" _/ |"));
-		console.log(chalk.blue("|__/"));
-        console.log(chalk.magenta("@version ") + pkg.version);
+        console.log(chalk.blue("   _     ______                    _   _       _"));
+        console.log(chalk.blue("  (_)   |  ____|                  | | (_)     | |"));
+        console.log(chalk.blue("   _ ___| |__   ___ ___  ___ _ __ | |_ _  __ _| |___"));
+        console.log(chalk.blue("  | / __|  __| / __/ __|/ _ \\ '_ \\| __| |/ _` | / __|"));
+        console.log(chalk.blue("  | \\__ \\ |____\\__ \\__ \\  __/ | | | |_| | (_| | \\__ \\"));
+        console.log(chalk.blue("  | |___/______|___/___/\\___|_| |_|\\__|_|\\__,_|_|___/"));
+        console.log(chalk.blue(" _/ |"));
+        console.log(chalk.blue("|__/"));
         console.log(chalk.magenta("@description ") + pkg.description);
         console.log(chalk.magenta("@author ") + pkg.author);
-		console.log(chalk.magenta("@homepage ") + pkg.homepage);
-		console.log(chalk.magenta("@repository ") + pkg.repository.url);
-		console.log(chalk.magenta("@license ") + pkg.license);
+        console.log(chalk.magenta("@homepage ") + pkg.homepage);
+        console.log(chalk.magenta("@repository ") + pkg.repository.url);
+        console.log(chalk.magenta("@license ") + pkg.license);
         console.log("");
         console.log(chalk.blue("Basic Commands"));
         console.log("==============");
