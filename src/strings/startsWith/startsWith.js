eac1ab926bce943dd115b96b552ac659da729a06
georapbox <georapbox@gmail.com>
2016-03-29 09:33:55 +0300

    Rename arguments

--- src/strings/startsWith/startsWith.js
+++ src/strings/startsWith/startsWith.js
@@ -1,18 +1,18 @@
 /**
  * Determines whether a string begins with the characters of another string,
  * returning true or false as appropriate.
- * @param {String} str The string to process.
- * @param {String} prefix The characters to be searched for at the start of the string.
+ * @param {String} subjectString The string to process.
+ * @param {String} searchString The characters to be searched for at the start of the string.
  * @param {Number} [position=0] The position in the original string at which to begin searching for `prefix`.
  * @returns {Boolean}
  */
-function startsWith(str, prefix, position) {
+function startsWith(subjectString, searchString, position) {
     'use strict';
 
-    str = str.toString();
-    prefix = prefix.toString();
+    subjectString = subjectString.toString();
+    searchString = searchString.toString();
     position = position || 0;
     return String.prototype.startsWith ?
-        str.startsWith(prefix, position) :
-        str.substr(position, prefix.length) === prefix;
+        subjectString.startsWith(searchString, position) :
+        subjectString.substr(position, searchString.length) === searchString;
 }
