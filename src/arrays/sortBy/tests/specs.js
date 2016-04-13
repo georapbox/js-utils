ef3e2ffad06125dcbb82913bf4e2fdd47a58e403
George Raptis <georapbox@gmail.com>
2016-04-13 14:27:58 +0300

    add tests

---
+++ src/arrays/sortBy/tests/specs.js
@@ -0,0 +1,43 @@
+/* global sortBy */
+
+(function () {
+    'use strict';
+
+    describe('arrays/sortBy', function () {
+        var people = [
+            {name: 'Elena', age: '2'},
+            {name: 'George', age: '33'},
+            {name: 'Vasiliki', age: '32'},
+            {name: 'Vaggelis', age: '30'}
+        ];
+
+        it('should sort people array by property name in ascending order', function () {
+            expect(sortBy(people, 'name')).toEqual([
+                {name: 'Elena', age: '2'},
+                {name: 'George', age: '33'},
+                {name: 'Vaggelis', age: '30'},
+                {name: 'Vasiliki', age: '32'}
+            ]);
+        });
+
+        it('should sort people array by property name in descending order', function () {
+            expect(sortBy(people, 'name', false)).toEqual([
+                {name: 'Vasiliki', age: '32'},
+                {name: 'Vaggelis', age: '30'},
+                {name: 'George', age: '33'},
+                {name: 'Elena', age: '2'}
+            ]);
+        });
+
+        it('should sort people array by property age in ascending order manipulating the age property', function () {
+            expect(sortBy(people, 'age', true, function (age) {
+                return age * 2;
+            })).toEqual([
+                {name: 'Elena', age: '2'},
+                {name: 'Vaggelis', age: '30'},
+                {name: 'Vasiliki', age: '32'},
+                {name: 'George', age: '33'}
+            ]);
+        });
+    });
+}());
