3769d0b72092d1ccb18a8076f36c6a2fcd99ce7f
georapbox <georapbox@gmail.com>
2016-05-03 09:38:51 +0300

    [ci-skip] add more tests

--- src/is/isArrayLike/tests/specs.js
+++ src/is/isArrayLike/tests/specs.js
@@ -4,18 +4,26 @@
     'use strict';
 
     describe('is/isArrayLike', function () {
+        function noop() {}
+
+        function dummy() {
+            return arguments;
+        }
+
         it('check if value is array-like', function () {
-            function noop() {}
+            var args = dummy();
 
-            function dummy(a, b) {
-                expect(isArrayLike(arguments)).toBe(true);
-            }
-
-            dummy();
+            expect(isArrayLike(args)).toBe(true);
 
             expect(isArrayLike([1, 2, 3])).toBe(true);
 
             expect(isArrayLike('abc')).toBe(true);
+
+            expect(isArrayLike(0)).toBe(false);
+
+            expect(isArrayLike({foo: 'bar'})).toBe(false);
+
+            expect(isArrayLike({foo: 'bar', length: 10})).toBe(true);
 
             expect(isArrayLike(noop)).toBe(false);
 
