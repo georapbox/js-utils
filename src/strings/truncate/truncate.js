52c6bb10074c027b5cb8e75d97124501403bd07b
George Raptis <georapbox@gmail.com>
2016-05-05 17:20:34 +0300

    [ci-skip] improve type checking

--- src/strings/truncate/truncate.js
+++ src/strings/truncate/truncate.js
@@ -2,8 +2,8 @@
  * Truncates a string based on character count.
  * @param {String} string The string to truncate.
  * @param {Number} characterCount The number of characters the truncated string contains. If ommitted or falsey, the initial string is returned.
- * @param {String} [delimiter="..."] The delimiter to add at the end of the truncated string. Optional. Default value is "...".
- * @returns {String} Returns the truncated string.
+ * @param {String} [delimiter=""] Defaults to empty string. The delimiter to add at the end of the truncated string. Optional.
+ * @return {String} Returns the truncated string.
  */
 function truncate(subjectString, characterCount, delimiter) {
     'use strict';
@@ -14,13 +14,19 @@ function truncate(subjectString, characterCount, delimiter) {
         throw new TypeError('Expected a string');
     }
 
-    if (characterCount > subjectString.length || !characterCount) {
+    if (typeof characterCount !== 'number') {
+        throw new TypeError('Expected a number');
+    }
+
+    characterCount = Math.floor(characterCount);
+
+    if (characterCount > subjectString.length || characterCount < 0 || characterCount !== characterCount) {
         return subjectString;
     }
 
     regex = new RegExp('^.{0,' + characterCount + '}[\S]*', 'g');
     truncated = subjectString.match(regex);
-    delimiter = delimiter || '...';
+    delimiter = delimiter || '';
     truncated = truncated[0].replace(/\s$/, '');
     truncated = truncated + delimiter;
 
