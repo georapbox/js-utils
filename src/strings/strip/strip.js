9ab2a2805d3f86ba6b880f49802def415e9eac99
georapbox <georapbox@gmail.com>
2016-05-05 10:42:17 +0300

    [ci-skip] type checking

--- src/strings/strip/strip.js
+++ src/strings/strip/strip.js
@@ -1,17 +1,24 @@
 /**
- * Returns a new string with all occurrences of [string1], [string2],... removed.
- * @param {String} str The initial string.
- * @returns {String} the final string.
+ * Creates a new string with all occurrences of [string1], [string2],... removed.
+ * @param {String} subjectString The string to process.
+ * @param {String} [...args] Optional. The substrings to remove from the original string.
+ * @returns {String} Returns the result string.
  */
-function strip(str) {
+function strip(subjectString /*...args*/) {
     'use strict';
 
-    var i = 1,
-        len = arguments.length;
+    var index, len;
 
-    for (i; i < len; i += 1) {
-        str = str.split(arguments[i]).join('');
+    if (typeof subjectString !== 'string') {
+        throw new TypeError('Expected a string');
     }
 
-    return str;
+    index = 1;
+    len = arguments.length;
+
+    for (index; index < len; index += 1) {
+        subjectString = subjectString.split(arguments[index]).join('');
+    }
+
+    return subjectString;
 }
