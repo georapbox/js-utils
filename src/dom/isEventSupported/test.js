55161c0142d24ac65bde884c964420ad0e536fa6
georapbox <georapbox@gmail.com>
2016-05-18 10:18:26 +0300

    add tests

---
+++ src/dom/isEventSupported/test.js
@@ -0,0 +1,23 @@
+/*global isEventSupported*/
+
+(function () {
+    'use strict';
+
+    describe('dom/isEventSupported', function () {
+        it('checks if event is supported by browser', function () {
+            expect(isEventSupported('submit')).toBe(true);
+
+            expect(isEventSupported('click')).toBe(true);
+
+            expect(isEventSupported('scroll')).toBe(true);
+
+            expect(isEventSupported('scroll', window)).toBe(true);
+
+            expect(isEventSupported('keyup')).toBe(true);
+
+            expect(isEventSupported('keyup', document.createElement('input'))).toBe(true);
+
+            expect(isEventSupported('something')).toBe(false);
+        });
+    });
+}());
