27d425728daa9266ab4d1aa0d5bb24e06ed1314e
georapbox <georapbox@gmail.com>
2016-05-24 09:51:40 +0300

    add dropRight function to Array category

---
+++ src/arrays/dropRight/README.md
@@ -0,0 +1,46 @@
+<a name="dropRight"></a>
+
+## dropRight(array, [n]) ⇒ <code>Array</code>
+Creates a slice of `array` with `n` elements dropped from the end.
+
+**Kind**: global function  
+**Returns**: <code>Array</code> - The slice of the array.  
+**Category**: Array  
+
+| Param | Type | Default | Description |
+| --- | --- | --- | --- |
+| array | <code>Array</code> |  | The array to process. |
+| [n] | <code>Number</code> | <code>1</code> | The number of elements to drop. |
+
+**Example**  
+```js
+dropRight([1, 2, 3, 4, 5]);
+// -> [1, 2, 3, 4]
+
+dropRight([1, 2, 3, 4, 5], null);
+// -> [1, 2, 3, 4]
+
+dropRight([1, 2, 3, 4, 5], NaN);
+// -> [1, 2, 3, 4]
+
+dropRight([1, 2, 3, 4, 5], 3);
+// -> [1, 2]
+
+dropRight([1, 2, 3, 4, 5], 0);
+// -> [1, 2, 3, 4, 5]
+
+dropRight([1, 2, 3, 4, 5], -2);
+// -> [1, 2, 3, 4, 5]
+
+dropRight([1, 2, 3, 4, 5], 10);
+// -> []
+
+dropRight([1, 2, 3, 4, 5], Number.MAX_VALUE);
+// -> []
+
+dropRight([1, 2, 3, 4, 5], '2');
+// -> Throws TypeError
+
+dropRight({}, 2);
+// -> Throws TypeError
+```
---
+++ src/arrays/dropRight/dropRight.js
@@ -0,0 +1,59 @@
+/**
+ * Creates a slice of `array` with `n` elements dropped from the end.
+ *
+ * @category Array
+ * @param {Array} array The array to process.
+ * @param {Number} [n=1] The number of elements to drop.
+ * @return {Array} The slice of the array.
+ * @example
+ *
+ * dropRight([1, 2, 3, 4, 5]);
+ * // -> [1, 2, 3, 4]
+ *
+ * dropRight([1, 2, 3, 4, 5], null);
+ * // -> [1, 2, 3, 4]
+ *
+ * dropRight([1, 2, 3, 4, 5], NaN);
+ * // -> [1, 2, 3, 4]
+ *
+ * dropRight([1, 2, 3, 4, 5], 3);
+ * // -> [1, 2]
+ *
+ * dropRight([1, 2, 3, 4, 5], 0);
+ * // -> [1, 2, 3, 4, 5]
+ *
+ * dropRight([1, 2, 3, 4, 5], -2);
+ * // -> [1, 2, 3, 4, 5]
+ *
+ * dropRight([1, 2, 3, 4, 5], 10);
+ * // -> []
+ *
+ * dropRight([1, 2, 3, 4, 5], Number.MAX_VALUE);
+ * // -> []
+ *
+ * dropRight([1, 2, 3, 4, 5], '2');
+ * // -> Throws TypeError
+ *
+ * dropRight({}, 2);
+ * // -> Throws TypeError
+ */
+function dropRight(array, n) {
+    'use strict';
+
+    var MAX_SAFE_INTEGER, length;
+
+    if (Object.prototype.toString.call(array) !== '[object Array]') {
+        throw new TypeError('Expected an array');
+    }
+
+    if (typeof n !== 'number' && n != null) {
+        throw new TypeError('Expected a number');
+    }
+
+    MAX_SAFE_INTEGER = Number.MAX_SAFE_INTEGER || Math.pow(2, 53) - 1;
+    length = array.length;
+    n = n > MAX_SAFE_INTEGER ? length : n == null || n !== n ? 1 : Math.floor(n);
+    n = length - n;
+
+    return array.slice(0, n < 0 ? 0 : n);
+}
