752cb4be24c710912bf7d777d7d18cdd42332838
George Raptis <georapbox@gmail.com>
2016-07-19 18:28:32 +0300

    [ci skip] isMap

---
+++ src/is/isMap/README.md
@@ -0,0 +1,27 @@
+<a name="isMap"></a>
+
+## isMap(value) ⇒ <code>Boolean</code>
+Checks if value is classified as a Map object.
+
+**Kind**: global function  
+**Returns**: <code>Boolean</code> - True if `value` is classified as a Map object, else false.  
+**Category**: Is  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| value | <code>\*</code> | The value to check. |
+
+**Example**  
+```js
+isMap(new Map());
+// -> true
+
+isMap(new WeakMap());
+// -> false
+
+isMap(new Map([['foo', 'bar'], ['hello', 'world']]));
+// -> true
+
+isMap({foo: 'bar', 'hello': 'world'});
+// -> false
+```
---
+++ src/is/isMap/isMap.js
@@ -0,0 +1,26 @@
+/**
+ * Checks if value is classified as a Map object.
+ *
+ * @category Is
+ * @param {*} value The value to check.
+ * @return {Boolean} True if `value` is classified as a Map object, else false.
+ * @example
+ *
+ * isMap(new Map());
+ * // -> true
+ *
+ * isMap(new WeakMap());
+ * // -> false
+ *
+ * isMap(new Map([['foo', 'bar'], ['hello', 'world']]));
+ * // -> true
+ *
+ * isMap({foo: 'bar', 'hello': 'world'});
+ * // -> false
+ */
+function isMap(value) {
+    'use strict';
+
+    return typeof value === 'object' &&
+        Object.prototype.toString.call(value) === '[object Map]';
+}
