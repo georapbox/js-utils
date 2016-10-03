54ee42b19fb6db50be70529446ed4126bd454bc6
George Raptis <georapbox@gmail.com>
2016-10-03 17:49:20 +0300

    Math/roundToNearest

---
+++ src/math/roundToNearest/README.md
@@ -0,0 +1,35 @@
+<a name="roundToNearest"></a>
+
+## roundToNearest(value, nearest) ⇒ <code>Number</code>
+Rounds a number to the nearest multiple of a value provided.
+
+**Kind**: global function  
+**Returns**: <code>Number</code> - The rounded number.  
+**Category**: Math  
+**Throws**:
+
+- <code>TypeError</code> If one or more of the arguments passed is not a number.
+
+
+| Param | Type | Default | Description |
+| --- | --- | --- | --- |
+| value | <code>Number</code> |  | The number to be rounded. |
+| nearest | <code>Number</code> | <code>1</code> | The value to be used in order to find the nearest multiple of. |
+
+**Example**  
+```js
+roundToNearest(100, 40);
+// -> 120
+
+roundToNearest(140, 40);
+// -> 160
+
+roundToNearest(180, 40);
+// -> 200
+
+roundToNearest(113, 0);
+// -> 113 (0 is converted to 1)
+
+roundToNearest(113, NaN);
+// -> 113 (NaN is converted to 1)
+```
---
+++ src/math/roundToNearest/roundToNearest.js
@@ -0,0 +1,36 @@
+/**
+ * Rounds a number to the nearest multiple of a value provided.
+ *
+ * @category Math
+ * @param {Number} value The number to be rounded.
+ * @param {Number} nearest=1 The value to be used in order to find the nearest multiple of.
+ * @throws {TypeError} If one or more of the arguments passed is not a number.
+ * @return {Number} The rounded number.
+ * @example
+ *
+ * roundToNearest(100, 40);
+ * // -> 120
+ *
+ * roundToNearest(140, 40);
+ * // -> 160
+ *
+ * roundToNearest(180, 40);
+ * // -> 200
+ *
+ * roundToNearest(113, 0);
+ * // -> 113 (0 is converted to 1)
+ *
+ * roundToNearest(113, NaN);
+ * // -> 113 (NaN is converted to 1)
+ */
+function roundToNearest(value, nearest) {
+  'use strict';
+
+  if (typeof value !== 'number' || typeof nearest !== 'number') {
+    throw new TypeError('all arguments of "roundToNearest" must be numbers');
+  }
+
+  nearest = nearest || 1;
+
+  return Math.round(value / nearest) * nearest;
+}
