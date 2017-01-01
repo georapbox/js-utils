ae2b380416a71a82346cd86994b2df181468de9a
georapbox <georapbox@gmail.com>
2017-01-01 09:39:22 +0200

    Improve test

--- src/arrays/uniq/test.js
+++ src/arrays/uniq/test.js
@@ -7,17 +7,9 @@
     var arr = ['a', 'a', 'b', true, '2', 2, false, true];
 
     it('removes duplicate primitive values from an array', function () {
-      var uArr = uniq(arr),
-        count = 0,
-        i, len;
+      var result = uniq(arr);
 
-      for (i = 0, len = arr.length; i < len; i++) {
-        if (uArr[i] === 'a') {
-          count += 1;
-        }
-      }
-
-      expect(count).toEqual(1);
+      expect(result).toEqual(['a', 'b', true, '2', 2, false]);
 
       expect(function () {
         return uniq({});
--- src/arrays/uniq/uniq.js
+++ src/arrays/uniq/uniq.js
@@ -13,29 +13,29 @@
 function uniq(array) {
   'use strict';
 
-  var newArr, len, i, j, found;
+  var duplicates, len, i, j, found;
 
   if (Object.prototype.toString.call(array) !== '[object Array]') {
     throw new TypeError('Expected an array');
   }
 
-  newArr = [];
+  duplicates = [];
   len = array.length;
 
   for (i = 0; i < len; i += 1) {
-    found = undefined;
+    found = false;
 
-    for (j = 0; j < newArr.length; j += 1) {
-      if (array[i] === newArr[j]) {
+    for (j = 0; j < duplicates.length; j += 1) {
+      if (array[i] === duplicates[j]) {
         found = true;
         break;
       }
     }
 
     if (!found) {
-      newArr.push(array[i]);
+      duplicates.push(array[i]);
     }
   }
 
-  return newArr;
+  return duplicates;
 }
