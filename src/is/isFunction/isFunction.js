82eab6d7922dfc580106bf4329ffc24e483f485c
George Raptis <georapbox@gmail.com>
2018-12-16 22:58:45 +0200

    update to cover cases for generators and async functions

--- src/is/isFunction/isFunction.js
+++ src/is/isFunction/isFunction.js
@@ -17,6 +17,9 @@
 function isFunction(value) {
   'use strict';
 
-  return typeof value === 'function' &&
-    Object.prototype.toString.call(value) === '[object Function]';
+  var tag = Object.prototype.toString.call(value);
+  var asyncTag = '[object AsyncFunction]';
+  var funcTag = '[object Function]';
+  var genTag = '[object GeneratorFunction]';
+  return tag === asyncTag || tag === funcTag || tag === genTag;
 }
