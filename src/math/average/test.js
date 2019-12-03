30eb7d57a154070e80fee27e70f4864656f2a442
georapbox <georapbox@gmail.com>
2019-12-03 10:20:56 +0200

    add more tests

--- src/math/average/test.js
+++ src/math/average/test.js
@@ -1,15 +1,30 @@
 describe('Math/average', function () {
   it('calculates the average of a set of numbers', function () {
-    expect(average()).toEqual(0);
+    expect(average(5, 10, 50, -45, 6, 7)).toBe(5.5);
+    expect(average([5, 10, 50, -45, 6, 7])).toBe(5.5);
 
-    expect(average(7)).toEqual(7);
+    expect(average(2, 4, 0, -0)).toBe(1.5);
+    expect(average([2, 4, 0, -0])).toBe(1.5);
 
-    expect(average(2, 4)).toEqual(3);
+    expect(average(7)).toBe(7);
+    expect(average([7])).toBe(7);
 
-    expect(average([2, 4])).toEqual(3);
+    expect(average(0, -0)).toBe(0);
+    expect(average([0, -0])).toBe(0);
 
-    expect(average(5, 10, 50, -45, 6, 7)).toEqual(5.5);
+    expect(average()).toBe(0);
+    expect(average([])).toBe(0);
 
-    expect(average(5, 4, null, true, '12', Infinity)).toEqual(4.5);
+    expect(average(Infinity, -Infinity, 0, -0, null, NaN, undefined, false, true, 'foo')).toBe(0);
+    expect(average([Infinity, -Infinity, 0, -0, null, NaN, undefined, false, true, 'foo'])).toBe(0);
+
+    expect(average(5, 4, null, true, '12', Infinity)).toBe(4.5);
+    expect(average([5, 4, null, true, '12', Infinity])).toBe(4.5);
+
+    expect(average([], {})).toBe(0);
+    expect(average([[], {}])).toBe(0);
+
+    expect(average('4', '2')).toBe(0);
+    expect(average(['4', '2'])).toBe(0);
   });
 });
