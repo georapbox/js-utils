11612b36932a8771b118a4e567a4a01f91e90c96
georapbox <georapbox@gmail.com>
2020-11-06 09:57:50 +0200

    Test escape sequences characters

--- packages/strings/trim/test.js
+++ packages/strings/trim/test.js
@@ -2,27 +2,25 @@ var trim = require('./trim');
 
 describe('String/trim', function () {
   function runTests() {
-    expect(trim('    Hello   ').length).toEqual(5);
+    expect(trim('    Hello   ')).toBe('Hello');
+
+    expect(trim('\t  Hello  \t')).toBe('Hello');
+
+    expect(trim('\n  Hello  \n')).toBe('Hello');
+
+    expect(trim('\v  Hello  \v')).toBe('Hello');
+
+    expect(trim('\r  Hello  \r')).toBe('Hello');
+
+    expect(trim('\t\n\v\r  Hello  \t\n\v\r')).toBe('Hello');
 
     expect(function () {
       return trim(null);
-    }).toThrow();
+    }).toThrow('Expected a string for first argument');
 
     expect(function () {
       return trim(123);
-    }).toThrow();
-
-    expect(function () {
-      return trim({});
-    }).toThrow();
-
-    expect(function () {
-      return trim([]);
-    }).toThrow();
-
-    expect(function () {
-      return trim(void 0);
-    }).toThrow();
+    }).toThrow('Expected a string for first argument');
   }
 
   it('trims string both ends (String.prototype.trim is uspported)', function () {
