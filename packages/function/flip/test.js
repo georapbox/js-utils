6f638ac5c7a7e6e9f23ac9cb31a9e774b45a8074
georapbox <georapbox@gmail.com>
2020-11-25 11:21:27 +0200

    Fix typo

--- packages/function/flip/test.js
+++ packages/function/flip/test.js
@@ -19,6 +19,6 @@ describe('Function/flip', function () {
 
     expect(function () {
       return flip(null)(1, 2);
-    }).toThrow('Expected a function for first argumen');
+    }).toThrow('Expected a function for first argument');
   });
 });
