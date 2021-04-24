0c08c4029d6865a08411100f9ab01dd67cc2b0ac
georapbox <georapbox@gmail.com>
2021-04-24 15:53:33 +0300

    add test

---
+++ packages/function/throttle/test.js
@@ -0,0 +1,36 @@
+var throttle = require('./throttle');
+
+describe('Function/throttle', function () {
+  beforeEach(function () {
+    jest.useFakeTimers();
+  });
+
+  afterEach(function () {
+    jest.clearAllTimers();
+  });
+
+  it('limits the number of times a function can be called in a given period', function () {
+    var func = jest.fn();
+
+    var throttled = throttle(func);
+
+    throttled();
+    throttled();
+
+    expect(setTimeout).toHaveBeenCalledTimes(1);
+
+    expect(func).toHaveBeenCalledTimes(1);
+
+    jest.advanceTimersByTime(200);
+
+    throttled();
+
+    expect(setTimeout).toHaveBeenCalledTimes(2);
+  });
+
+  it('throws if first argument is not a function', function () {
+    expect(function () {
+      return throttle();
+    }).toThrow(new TypeError('Expected a function for first argument'));
+  });
+});
